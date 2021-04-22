1f071a959ad8864a2f6b7321026bd20efb7ab993
rafaelgckto <rafael.gcecatto@gmail.com>
2021-04-22 11:42:19 -0300

    adição do projeto e enunciado

---
+++ RPG_character/Personagem.cs
@@ -0,0 +1,93 @@
+﻿namespace RPG_character {
+    class Personagem {
+        // Personagem: level, exp, mana.
+        int level = 1, exp, mana;
+
+        // Skills : level, cost.
+        int[] levelSkill = new int[4];
+        int[] costSkills = new int[4];
+
+        int pointsSkills = 0;
+
+        bool firstLevel = true;
+
+        int manaMax;
+
+        public Personagem(int qtdManaMax, int costSkill1, int costSkill2, int costSkill3, int costSkill4) {
+            mana = qtdManaMax;
+            costSkills[0] = costSkill1;
+            costSkills[1] = costSkill2;
+            costSkills[2] = costSkill3;
+            costSkills[3] = costSkill4;
+
+            manaMax = qtdManaMax;
+        }
+
+        // Adiciona uma quantidade de pontos de experiência ao personagem.
+        public void AdicionarXp(int pointsExp) {
+            exp += pointsExp;
+
+            while(exp > 100 && level < 25) {
+                level++;
+                exp -= 100;
+            }
+
+            if(level == 25 && exp > 100)
+                exp = 0;
+
+            if(exp == 100) {
+                level++;
+                pointsSkills++;
+                exp = 0;
+            }
+        }
+
+        // Retorna o nível atual do personagem
+        public int GetNivel() {
+            return level;
+        }
+
+        // Melhora uma das quatro habilidades do personagem(indexada por 0).
+        // Retorna um booleano indicando se foi possível melhorá-la.
+        public bool MelhorarHabilidade(int skill) {
+            if(level < 6 && skill == 3)
+                return false;
+
+            if((levelSkill[skill] == 4 && skill < 3) || (levelSkill[skill] == 3 && skill == 3))
+                return false;
+
+            if(pointsSkills > 0 || firstLevel) {
+                levelSkill[skill] += 1;
+                firstLevel = false;
+                return true;
+            }
+
+            return false;
+        }
+
+        // Ativa a habilidade do personagem (indexada por 0), consumindo mana no processo.
+        // Retorna um booleano indicando se foi possível usar a habilidade.
+        public bool UsarHabilidade(int skill) {
+            if(levelSkill[skill] > 0) {
+                int test = costSkills[skill] * levelSkill[skill];
+                if(mana >= test) {
+                    mana -= test;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        // Recarrega a mana do personagem em 350.
+        // A mana total não pode ultrapassar a mana máxima.
+        public void ConsumirPocao() {
+            mana += 350;
+
+            if(mana > manaMax) {
+                int difference = mana - manaMax;
+                mana -= difference;
+            }
+        }
+    }
+}
---
+++ RPG_character/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace RPG_character {
+    class Program {
+        static void Main(string[] args) {
+            //testarReacao();
+            Console.WriteLine();
+            testarPersonagem();
+            Console.ReadLine();
+        }
+
+        static void testarPersonagem() {
+            int totalTestes = 24;
+            int testesCorretos = 0;
+
+            Console.WriteLine("### Personagem\n");
+
+            Personagem p = new Personagem(500, 70, 100, 10, 200);
+            testesCorretos += rodarTeste("Nivel inicia em 1", p.GetNivel() == 1);
+            testesCorretos += rodarTeste("Nivel 1 pode melhorar habilidade", p.MelhorarHabilidade(0));
+            testesCorretos += rodarTeste("Nivel 1 pode melhorar apenas uma habilidade", !p.MelhorarHabilidade(0));
+            p.AdicionarXp(100);
+            testesCorretos += rodarTeste("Personagem pode subir de nivel", p.GetNivel() == 2);
+            testesCorretos += rodarTeste("Personagem pode melhorar outra habilidade", p.MelhorarHabilidade(1));
+            p.AdicionarXp(50);
+            p.AdicionarXp(50);
+            testesCorretos += rodarTeste("Personagem sobe de nivel mesmo recebendo experiencia aos poucos", p.GetNivel() == 3);
+            testesCorretos += rodarTeste("Personagem nao pode melhorar ultimate antes do nivel 6", !p.MelhorarHabilidade(3));
+            p.AdicionarXp(300);
+            testesCorretos += rodarTeste("Personagem pode chegar no nivel 6", p.GetNivel() == 6);
+            testesCorretos += rodarTeste("Personagem pode melhorar ultimate no nivel 6", p.MelhorarHabilidade(3));
+            testesCorretos += rodarTeste("Personagem nao pode usar habilidade com ni­vel de melhoria 0", !p.UsarHabilidade(2));
+            p.MelhorarHabilidade(2); // Até aqui: Nível do Personagem: 6. Níveis das Habilidades: { 1, 1, 1, 1 }.
+            testesCorretos += rodarTeste("Personagem pode usar habilidade com ni­vel de melhoria > 0", p.UsarHabilidade(2)); // Mana: 490/500.
+
+            p.UsarHabilidade(3); // Mana: 290/500.
+            p.UsarHabilidade(3); // Mana: 90/500.
+
+            testesCorretos += rodarTeste("Personagem nao pode usar habilidade se nao tiver mana suficiente", !p.UsarHabilidade(3));
+
+            // Poção
+            p.ConsumirPocao(); // Mana: 440/500.
+            testesCorretos += rodarTeste("Personagem pode recuperar mana com pocao", p.UsarHabilidade(3)); // Mana: 240/500.
+            p.ConsumirPocao(); // Mana: 500/500.
+            p.ConsumirPocao(); // tomar outras vezes nao deveria deixar passar dos 500
+            p.ConsumirPocao();
+            p.ConsumirPocao(); // Mana: 500/500.
+
+            p.UsarHabilidade(3); // Mana: 300/500.
+            p.UsarHabilidade(3); // Mana: 100/500.
+            p.UsarHabilidade(1); // Mana: 0/500.
+
+            testesCorretos += rodarTeste("Pocao nao recupera alem da mana maxima", !p.UsarHabilidade(2));
+
+            // Niveis máximos
+            p.AdicionarXp(2500);
+            testesCorretos += rodarTeste("Nao e possi­vel passar do ni­vel 25", p.GetNivel() == 25);
+            // Níveis das Habilidades: { 1, 1, 1, 1 }.
+            p.MelhorarHabilidade(0); // Níveis das Habilidades: { 2, 1, 1, 1 }.
+            p.MelhorarHabilidade(0); // Níveis das Habilidades: { 3, 1, 1, 1 }.
+            testesCorretos += rodarTeste("Habilidade 0 chega ao ni­vel 4", p.MelhorarHabilidade(0)); // Níveis das Habilidades: { 4, 1, 1, 1 }.
+            testesCorretos += rodarTeste("Habilidade 0 nao passa do nivel 4", !p.MelhorarHabilidade(0));
+            p.MelhorarHabilidade(1);
+            p.MelhorarHabilidade(1);
+            testesCorretos += rodarTeste("Habilidade 1 chega ao nivel 4", p.MelhorarHabilidade(1));
+            testesCorretos += rodarTeste("Habilidade 1 nao passa do ni­vel 4", !p.MelhorarHabilidade(1));
+            p.MelhorarHabilidade(2);
+            p.MelhorarHabilidade(2);
+            testesCorretos += rodarTeste("Habilidade 2 chega ao nivel 4", p.MelhorarHabilidade(2));
+            testesCorretos += rodarTeste("Habilidade 2 nao passa do nivel 4", !p.MelhorarHabilidade(2));
+            p.MelhorarHabilidade(3);
+            testesCorretos += rodarTeste("Habilidade 3 chega ao nivel 3", p.MelhorarHabilidade(3));
+            testesCorretos += rodarTeste("Habilidade 3 nao passa do nivel 3", !p.MelhorarHabilidade(3));
+
+            p.ConsumirPocao();
+            p.ConsumirPocao();
+            // mesmo os 500 de mana nao sao suficientes para usar a ult no nivel 3 (200*3 = 600)
+            testesCorretos += rodarTeste("Consumo de mana Ã© proporcional ao ni­vel da habilidade", !p.UsarHabilidade(3));
+
+            mostrarResultado(testesCorretos, totalTestes);
+        }
+
+        static int rodarTeste(String titulo, bool resultado) {
+            Console.WriteLine("- " + (resultado ? "OK" : "X ") + "\t" + titulo);
+            return resultado ? 1 : 0;
+        }
+
+        static void mostrarResultado(int testesCorretos, int totalTestes) {
+            Console.WriteLine("\n> Testes corretos: " + testesCorretos + "/" + totalTestes + " (" + (100 * testesCorretos / totalTestes) + "%)");
+            if(testesCorretos == totalTestes) {
+                Console.WriteLine("> Tudo certo!!!");
+            }
+            else {
+                Console.WriteLine(">  Ainda falta um pouquinho, mas voce consegue!");
+            }
+        }
+    }
+}
